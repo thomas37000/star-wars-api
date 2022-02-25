d764cc79cea233cb3c04c87a704177e25a61574a
thomas <thomas.chalanson@sfr.fr>
2022-02-25 18:50:33 +0100

    do a grid to map all the characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -32,5 +32,9 @@ export default function Character() {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error...</div>;
 
-  return <div>{data && data.map((all, i) => <Card key={i} {...all} />)}</div>;
+  return (
+    <div className="max-w-2xl mx-auto px-4 py-8 lg:max-w-7xl grid grid-cols-1 gap-y-10 gap-x-8 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4">
+      {data && data.map((all, i) => <Card key={i} {...all} />)}
+    </div>
+  );
 }

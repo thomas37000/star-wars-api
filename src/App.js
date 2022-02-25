4f7173713ca72c9b1a8d714826ed6d6a65636913
thomas <thomas.chalanson@sfr.fr>
2022-02-25 19:20:06 +0100

    do a nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Characters from "./components/Characters";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Navbar />
       <Characters />
     </div>
   );
